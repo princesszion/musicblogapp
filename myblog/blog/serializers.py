77928853905469408d82ad7fa8e5eb5de39a0dc8
princesszion <maikemvictorine@gmail.com>
2024-05-20 13:14:49 +0200

    added user to comments

--- myblog/blog/serializers.py
+++ myblog/blog/serializers.py
@@ -1,36 +1,11 @@
 # from rest_framework import serializers
 # from .models import BlogPost, Comment
 
-# # class BlogPostSerializer(serializers.ModelSerializer):
-# #     class Meta:
-# #         model = BlogPost
-# #         fields = ['id', 'title', 'content', 'thumbnail', 'created_at', 'updated_at', 'author']
-
-# # class BlogPostSerializer(serializers.ModelSerializer):
-# #     class Meta:
-# #         model = BlogPost
-# #         fields = ['title', 'content', 'thumbnail', 'created_at', 'updated_at', 'author']
-# #         read_only_fields = ['author']  # Ensure author is not expected from request data
-
-# #     def create(self, validated_data):
-# #         validated_data['author'] = self.context['request'].user
-# #         return BlogPost.objects.create(**validated_data)
-
-
 # class BlogPostSerializer(serializers.ModelSerializer):
-#     thumbnail = serializers.SerializerMethodField()
-
 #     class Meta:
 #         model = BlogPost
 #         fields = ['id', 'title', 'content', 'thumbnail', 'created_at', 'updated_at', 'author']
-#         read_only_fields = ['author']  # Ensure author is not expected from request data
-
-#     def get_thumbnail(self, obj):
-#         request = self.context.get('request')
-#         if obj.thumbnail:
-#             thumbnail_url = obj.thumbnail.url
-#             return request.build_absolute_uri(thumbnail_url)
-#         return None
+#         read_only_fields = ['author']
 
 #     def create(self, validated_data):
 #         validated_data['author'] = self.context['request'].user
@@ -40,6 +15,14 @@
 #     class Meta:
 #         model = Comment
 #         fields = ['id', 'post', 'author', 'content', 'created_at', 'updated_at']
+#         read_only_fields = ['author', 'post']
+
+#     def create(self, validated_data):
+#         validated_data['author'] = self.context['request'].user
+#         post = BlogPost.objects.get(pk=self.context['post_pk'])
+#         validated_data['post'] = post
+#         return Comment.objects.create(**validated_data)
+
 
 
 
@@ -57,9 +40,11 @@ class BlogPostSerializer(serializers.ModelSerializer):
         return BlogPost.objects.create(**validated_data)
 
 class CommentSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')
+
     class Meta:
         model = Comment
-        fields = ['id', 'post', 'author', 'content', 'created_at', 'updated_at']
+        fields = ['id', 'post', 'author', 'author_username', 'content', 'created_at', 'updated_at']
         read_only_fields = ['author', 'post']
 
     def create(self, validated_data):
@@ -67,3 +52,4 @@ class CommentSerializer(serializers.ModelSerializer):
         post = BlogPost.objects.get(pk=self.context['post_pk'])
         validated_data['post'] = post
         return Comment.objects.create(**validated_data)
+
