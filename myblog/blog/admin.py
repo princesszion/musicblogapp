e4920fe40eff05c3d3ca8646395cdf79df0f93a6
princesszion <maikemvictorine@gmail.com>
2025-05-06 16:01:03 -0400

    added events to admin

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import BlogPost, Comment
+from .models import BlogPost, Comment, Event
 
 admin.site.register(BlogPost)
 admin.site.register(Comment)
+admin.site.register(Event)
